54b29f4cb06bc60e1db2bab7dc34b1ed4d4872d0
Clemens Mol <mail@clemensmol.nl>
2018-09-14 12:41:03 +0200

    iPad support
    Requires a view in React Native with nativeID="OnePassword" to work.

--- OnePassword.m
+++ OnePassword.m
@@ -2,13 +2,35 @@
 #import "OnePasswordExtension.h"
 
 #import <React/RCTUtils.h>
+#import <LocalAuthentication/LocalAuthentication.h>
+#import <React/RCTUIManager.h>
+
+#import <UIKit/UIKit.h>
+#import "UIView+React.h"
+
+@implementation UIView (Recurrence)
+
+- (NSArray<UIView *> *)recurrenceAllSubviews
+{
+    NSMutableArray <UIView *> *all = @[].mutableCopy;
+    void (^getSubViewsBlock)(UIView *current) = ^(UIView *current){
+
+        for (UIView *sub in current.subviews) {
+
+            if ([sub.nativeID  isEqual: @"OnePassword"]) {
+                [all addObject:sub];
+            }
+
+            [all addObjectsFromArray:[sub recurrenceAllSubviews]];
+        }
+    };
+    getSubViewsBlock(self);
+    return [NSArray arrayWithArray:all];
+}
+@end
+
 
 @implementation OnePassword
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
 
 RCT_EXPORT_MODULE();
 
@@ -29,8 +51,20 @@ RCT_EXPORT_METHOD(findLogin: (NSString *)url
     UIViewController *controller = RCTPresentedViewController();
     
     dispatch_async(dispatch_get_main_queue(), ^{
+        UIView *sender = nil;
+
+        // Loop through all subviews to find a view with nativeID "OnePassword"
+        UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+        for(UIView *subview in keyWindow.subviews)
+        {
+            for (UIView *subsubview in [subview recurrenceAllSubviews])
+            {
+                sender = subsubview;
+            }
+        }
+        
         [[OnePasswordExtension sharedExtension] findLoginForURLString:url
-        forViewController:controller sender:nil completion:^(NSDictionary *loginDictionary, NSError *error) {
+        forViewController:controller sender:sender completion:^(NSDictionary *loginDictionary, NSError *error) {
             if (loginDictionary.count == 0) {
                 callback(@[RCTMakeError(@"Error while getting login credentials.", nil, nil)]);
                 return;
